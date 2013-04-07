048b74b2e81510f213f664f45dd3093801724fe2
Alexey Ulyanov <sibuser.nsk@gmail.com>
2013-04-08 00:58:49 +0200

    added regExp for param

--- parser.py
+++ parser.py
@@ -18,19 +18,23 @@ for line in f:
 
         for line2 in f:
             if 'brief' in line2:
-                startString = ' * \\brief   '
-                endString = re.match(r'(.*)brief(\s*)(.*)', line2)
-                buf.append(startString + endString.group(3) + '\n')
+                matcher = re.match(r'(.*)brief(\s*)(.*)', line2)
+                buf.append(' * \\brief   ' + matcher.group(3) + '\n')
                 continue
             if 'details' in line2:
-                startString = ' * \\details '
-                endString = re.match(r'(.*)details(\s*)(.*)', line2)
-                buf.append(startString + endString.group(3) + '\n')
+                matcher = re.match(r'(.*)details(\s*)(.*)', line2)
+                buf.append(' * \\details ' + matcher.group(3) + '\n')
                 continue
             if 'details' in line2:
-                startString = ' * \\details '
-                endString = re.match(r'(.*)details(\s*)(.*)', line2)
-                buf.append(startString + endString.group(3) + '\n')
+                matcher = re.match(r'(.*)details(\s*)(.*)', line2)
+                buf.append(' * \\details ' + matcher.group(3) + '\n')
+                continue
+            if 'param' in line2:
+                matcher = re.match(r'(.*)param(\s*)(\[.*\])(\s*)(\w*)(\s+)(.*)', line2)
+                firstPart = ' * \\param ' + matcher.group(3) + matcher.group(4) + matcher.group(5)
+                buf.append( firstPart +
+                           ' ' * (31 - len(firstPart)) +
+                           matcher.group(7) + '\n')
                 continue
             else:
                 buf.append(line2)
