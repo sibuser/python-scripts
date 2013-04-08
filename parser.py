d8d44a7bbd6345b66e4d9be6ecfcc42c4012c455
Alexey Ulyanov <sibuser.nsk@gmail.com>
2013-04-09 00:35:47 +0200

    Moved common code into functions.

--- parser.py
+++ parser.py
@@ -7,12 +7,31 @@ if len(sys.argv) < 1:
 else:
     f = open('test.cc', 'r')
 buf = []
+intend = 0
 
-def parseString():
-    print('dfg')
+def briefDetails(buf, string):
+    matcher = re.match(r'(\s\*\s)(\\\w+)(\s*)(.*)', string)
+    firstPart = ' * ' + matcher.group(2)
+    buf.append(firstPart + ' ' * (12 - len(firstPart)) + matcher.group(4) + '\n')
+    intend = 9
+
+def restPart(buf, string):
+    matcher = re.match(r'(\s\*\s)(\\\w+)(\s*)(\w*)(\s+)(.*)', string)
+    firstPart = ' * ' + matcher.group(2) + ' ' + matcher.group(4)
+    buf.append(firstPart + ' ' * (31 - len(firstPart)) + matcher.group(6) + '\n')
+    intend = 29
+
+def param(buf, string):
+    matcher = re.match(r'(\s\*\s)(\\\w+)(\s*)(\[.*\])(\s*)(\w*)(\s+)(.*)', string)
+    firstPart = ' * ' + matcher.group(2) + ' ' + matcher.group(4) + ' ' + matcher.group(6)
+    buf.append( firstPart +
+               ' ' * (31 - len(firstPart)) +
+               matcher.group(8) + '\n')
+    intend = 29
 
 for line in f:
     if '/**' in line:
+
         # if len(line) is not 4:
         #     tmp = list(line)
         #     buf.append(''.join(tmp[0:3]) + '\n')
@@ -21,25 +40,26 @@ for line in f:
         buf.append(line)
 
         for line2 in f:
-            if 'brief' in line2:
-                matcher = re.match(r'(.*)brief(\s*)(.*)', line2)
-                buf.append(' * \\brief   ' + matcher.group(3) + '\n')
+
+            if '\\brief' in line2:
+                briefDetails(buf,line2)
                 continue
-            if 'details' in line2:
-                matcher = re.match(r'(.*)details(\s*)(.*)', line2)
-                buf.append(' * \\details ' + matcher.group(3) + '\n')
+            elif '\details' in line2:
+                briefDetails(buf,line2)
                 continue
-            if 'details' in line2:
-                matcher = re.match(r'(.*)details(\s*)(.*)', line2)
-                buf.append(' * \\details ' + matcher.group(3) + '\n')
+            elif '\pre' in line2:
+                briefDetails(buf,line2)
                 continue
-            if 'param' in line2:
-                matcher = re.match(r'(.*)param(\s*)(\[.*\])(\s*)(\w*)(\s+)(.*)', line2)
-                firstPart = ' * \\param ' + matcher.group(3) + matcher.group(4) + matcher.group(5)
-                buf.append( firstPart +
-                           ' ' * (31 - len(firstPart)) +
-                           matcher.group(7) + '\n')
-                continue
+            elif '\\return' in line2:
+                restPart(buf,line2)
+            elif '\\retval' in line2:
+                restPart(buf,line2)
+            elif '\exception' in line2:
+                restPart(buf,line2)
+            elif '\\remark' in line2:
+                restPart(buf,line2)
+            elif '\param' in line2:
+                param(buf,line2)
             else:
                 buf.append(line2)
             if "*/" in line2: break
