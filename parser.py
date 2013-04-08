881df6093d00a439abda18ee49cfb99326d7fb40
Alexey Ulyanov <sibuser.nsk@gmail.com>
2013-04-09 01:11:49 +0200

    Fixed alignment.

--- parser.py
+++ parser.py
@@ -9,25 +9,32 @@ else:
 buf = []
 intend = 0
 
-def briefDetails(buf, string):
+def briefDetails(buf, string, amountOfSpaces = 12):
     matcher = re.match(r'(\s\*\s)(\\\w+)(\s*)(.*)', string)
     firstPart = ' * ' + matcher.group(2)
-    buf.append(firstPart + ' ' * (12 - len(firstPart)) + matcher.group(4) + '\n')
-    intend = 9
+    buf.append(firstPart + ' ' * (amountOfSpaces - len(firstPart)) + matcher.group(4) + '\n')
+    global intend
+    intend = 10
 
-def restPart(buf, string):
+def restPart(buf, string, amountOfSpaces = 31):
     matcher = re.match(r'(\s\*\s)(\\\w+)(\s*)(\w*)(\s+)(.*)', string)
     firstPart = ' * ' + matcher.group(2) + ' ' + matcher.group(4)
-    buf.append(firstPart + ' ' * (31 - len(firstPart)) + matcher.group(6) + '\n')
+    buf.append(firstPart + ' ' * (amountOfSpaces - len(firstPart)) + matcher.group(6) + '\n')
+    global intend
     intend = 29
 
-def param(buf, string):
+def param(buf, string, amountOfSpaces = 31):
     matcher = re.match(r'(\s\*\s)(\\\w+)(\s*)(\[.*\])(\s*)(\w*)(\s+)(.*)', string)
     firstPart = ' * ' + matcher.group(2) + ' ' + matcher.group(4) + ' ' + matcher.group(6)
     buf.append( firstPart +
-               ' ' * (31 - len(firstPart)) +
+               ' ' * (amountOfSpaces - len(firstPart)) +
                matcher.group(8) + '\n')
+    global intend
     intend = 29
+
+def alignment(buf, string):
+    matcher = re.match(r'(\s\*)(\s*)(.*)', string)
+    buf.append(' *' + ' ' * intend + matcher.group(3) + '\n')
 
 for line in f:
     if '/**' in line:
@@ -57,9 +64,11 @@ for line in f:
             elif '\exception' in line2:
                 restPart(buf,line2)
             elif '\\remark' in line2:
-                restPart(buf,line2)
+                restPart(buf,line2, 1)
             elif '\param' in line2:
                 param(buf,line2)
+            elif re.match(r'\s\*\s{3,}', line2):
+                alignment(buf,line2)
             else:
                 buf.append(line2)
             if "*/" in line2: break
