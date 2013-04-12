d5e204b5270faaff5eff56e8069829b62e81fa9c
Alexey Ulyanov <sibuser.nsk@gmail.com>
2013-04-12 02:02:48 +0200

    WIP. more comments. use verbose for regExp with comments. Fixed bug in param call. Add one empty string between new tag and last string of comments.

--- parser.py
+++ parser.py
@@ -19,72 +19,138 @@ src = open(args.src, "r+")
 
 buf = []
 
-intend = 0
+intend = 0 #for alignment multi string descriptions
+
 '''
 Function searches the string in format:
 * \tag + description
 * and set the indentation upon multi string description.
 '''
 def tagDescr(buf, string, amountOfSpaces = 12):
-    matcher = re.match(r"(\s\*\s)(\\\w+)(\s*)(.*)", string)
-    firstPart = " * " + matcher.group(2)
-    buf.append(firstPart + " " * (amountOfSpaces - len(firstPart)) + matcher.group(4) + "\n")
+    # add one empty string between next tag and last string of description
+    if '*\n' not in buf[len(buf) - 1]:
+        buf.append(' * \n')
+
+    matcher = re.compile(r"""
+        (\s\*\s) # all spaces and asterisks before tag
+        (\\\w+)  # tag itself
+        (\s+)    # all spaces between tag and description
+        (.*)     # description
+        """, re.VERBOSE)
+    result = matcher.match(string)
+    if result:
+        firstPart = " * " + result.group(2)
+
+        if len(firstPart) < 13:
+            buf.append(firstPart + " " * (amountOfSpaces - len(firstPart)) + result.group(4) + "\n")
+        # if tag and name of argument are longer then 13 char we will add only one
+        # space between tag and description.
+        else:
+            buf.append(firstPart + " " + result.group(4) + "\n")
+
     global intend
     intend = 10
+
 '''
 Function searches the string in format:
 * \tag + argument + description
 * and set the indentation upon multi string description.
 '''
 def tagArgDescr(buf, string, amountOfSpaces = 31):
-    matcher = re.match(r"(\s\*\s)(\\\w+)(\s*)(\w*)(\s+)(.*)", string)
-    firstPart = " * " + matcher.group(2) + " " + matcher.group(4)
-    if len(firstPart) < 31:
-        buf.append(firstPart + " " * (amountOfSpaces - len(firstPart)) + matcher.group(6) + "\n")
-    else:
-        buf.append(firstPart + "\n *" + ' ' * (amountOfSpaces - 2) +
-                    matcher.group(6) + "\n")
+    if '*\n' not in buf[len(buf) - 1]:
+        buf.append(' * \n')
+
+    matcher = re.compile(r"""
+        (\s\*\s)    # all spaces and asterisks before tag
+        (\\\w+)     # tag itself
+        (\s+)       # all spaces
+        (\w+)       # name of argument
+        (\s+)       # all spaces between tag and description
+        (.*)        # description
+        """, re.VERBOSE)
+    result = matcher.match(string)
+    if result:
+        firstPart = " * " + result.group(2) + " " + result.group(4)
+        if len(firstPart) < 31:
+            buf.append(firstPart + " " * (amountOfSpaces - len(firstPart)) + result.group(6) + "\n")
+        else:
+            buf.append(firstPart + "\n *" + ' ' * (amountOfSpaces - 2) +
+                        result.group(6) + "\n")
     global intend
     intend = 29
 '''
 Function searches the string in format:
- * \tag [in/out] argument description
+* \tag [in/out] argument description
 * and set the indentation upon multi string description.
 '''
 def tagTwoArgDescr(buf, string, amountOfSpaces = 31):
-    #                 (r"( * )(first word)(all spaces)([in or out])(all spaces)
-    #                 (name of argument)(spaces)(last part of string)")
-    matcher = re.match(r"(\s\*\s)(\\\w+)(\s*)(\[.*\])(\s*)(\w*)(\s+)(.*)", string)
-    firstPart = " * " + matcher.group(2) + " " + matcher.group(4) + " " + matcher.group(6)
-    if len(firstPart) > 31:
-        buf.append(firstPart + "\n *" + ' ' * (amountOfSpaces - 2) +
-                    matcher.group(8) + "\n")
-    else:
-        buf.append(firstPart + " " * (amountOfSpaces - len(firstPart)) +
-                    matcher.group(8) + "\n")
-    # else:
+    if '*\n' not in buf[len(buf) - 1]:
+        buf.append(' * \n')
+
+    matcher = re.compile(r"""
+        (\s\*\s) # all spaces and asterisks before tag
+        (\\\w+)
+        (\s+)
+        (
+            (\w+)    # name of argument
+            (\s+)
+            (\[.+\])|
+            (\[.+\])
+            (\s+)
+            (\w+)
+        )
+        (\s+)    # all spaces between tag and description
+        (.*)     # description
+        """, re.VERBOSE)
+
+    result = matcher.match(string)
+    if result:
+        IN_OUT   = (8,7)[result.group(8) == None]
+        ARG_NAME = (10,5)[result.group(8) == None]
+
+        firstPart = " * " + result.group(2) + " " + result.group(IN_OUT) + " " + result.group(ARG_NAME)
+        if len(firstPart) > 31:
+            buf.append(firstPart + "\n *" + ' ' * (amountOfSpaces - 2) +
+                        result.group(12) + "\n")
+        else:
+            buf.append(firstPart + " " * (amountOfSpaces - len(firstPart)) +
+                        result.group(12) + "\n")
     global intend
     intend = 29
 '''
-Alignments all string of multi string descroption.
+Alignments all string of multi string descriptions.
 '''
 def alignment(buf, string):
-    matcher = re.match(r"(\s\*)(\s*)(.*)", string)
-    buf.append(" *" + " " * intend + matcher.group(3) + "\n")
+    matcher = re.compile(r"""
+        (\s+\*\s+)    # all spaces and asterisks before description
+        (.+)          # description
+        """, re.VERBOSE)
+    result = matcher.match(string)
+
+    if result:
+        buf.append(" *" + " " * intend + result.group(2) + "\n")
 
 for line in src:
     if "/**" in line:
-        # if first string of comments longer than 4 characters
+        # if first string of comments contains anything except /**
         # we will split it on two strings.
         if len(line) > 4:
-            matcher = re.match(r"(\/\**)(\\\w.*)", line)
-            buf.append('/**\n')
-            tagDescr(buf, ' * ' + matcher.group(2))
+            matcher = re.compile(r"""
+                (\s*/\*+\s+) # search /** in any variants
+                (\\\w+.*)    # take any symbols when tag is appeared
+                """,re.VERBOSE)
+            result = matcher.match(line)
+            if result:
+                buf.append('/**\n')
+                tagDescr(buf, ' * ' + result.group(2))
         else:
             buf.append(line)
 
+
         for line2 in src:
             if "\\brief" in line2:
+                tagDescr(buf,line2)
+            elif "\\attention" in line2:
                 tagDescr(buf,line2)
             elif "\details" in line2:
                 tagDescr(buf,line2)
@@ -98,7 +164,7 @@ for line in src:
                 tagArgDescr(buf,line2)
             elif "\\remark" in line2:
                 tagArgDescr(buf,line2, 1)
-            elif "\tagTwoArgDescr" in line2:
+            elif "\\param" in line2:
                 tagTwoArgDescr(buf,line2)
             elif "\code" in line2:
                 # add all strings from \code block into buffer as unchanged
@@ -106,7 +172,7 @@ for line in src:
                 for i in src:
                     buf.append(i)
                     if "\endcode" in i: break
-            elif re.match(r"\s\*\s{3,}[\w]+", line2):
+            elif re.match(r"\s\*\s+[\w]+", line2):
                 alignment(buf,line2)
             else:
                 buf.append(line2)
